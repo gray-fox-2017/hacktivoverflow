cc0df40acf00abc56a07bdf0b162df4e3db2aeeb
erwinwahyura <you@example.com>
2017-06-08 09:55:36 +0700

    adding collection

---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var validator = require("email-validator");
+
+var userSchema = new Schema({
+  username:  {
+    type : String,
+    required : [true, 'username is required! please fill it!'],
+    unique : [true, 'username already used by another user']
+  },
+  password: {
+    type : String,
+    required : [true, 'password is required! please fill it!']
+  },
+  email: {
+    type : String,
+    required : [true, 'email is required! please fill it!'],
+    validator.validate_async(function(err, isValidEmail) {
+        //Do something with isValidEmail
+        validator.validate(isValidEmail)
+    }); // true
+
+  }
+});
+
+var user = mongoose.model('User', userSchema);
+
+module.exports = user
