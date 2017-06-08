69d873aebed79caba4daae7dfa8984844995770d
erwinwahyura <you@example.com>
2017-06-08 09:23:14 +0700

    add client named hacktiv-overflow

---
+++ hactiv-overflow/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- for interface in client web browser -->
+    <Navbar></Navbar>
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+// if u want to use import the component
+
+import Navbar from '@/components/Navbar'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+    // ini daftarin components yang mau di pake
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
---
+++ hactiv-overflow/src/components/Auth.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          
+        </div>
+        <div class="col-md-4">
+          <b-form-input v-model="name" type="text" placeholder="Enter your name" :state="text.length?'success':'warning'" :formatter="format"></b-form-input>
+          <small class="text-muted">We will convert your name to lowercase instantly</small>
+
+          <br>
+          <br>
+
+          <b-form-input v-model="email" type="text" placeholder="Enter your name" :state="text.length?'success':'warning'" :formatter="format" lazy-formatter></b-form-input>
+            <small class="text-muted">This one is a little lazy!</small>
+
+          <p>Value: {{text}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
