761a46ba9cd0a09cbf66619c11930d0afa17eb22
erwinwahyura <you@example.com>
2017-06-11 15:30:40 +0700

    auth done

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,65 +1,97 @@
-const secret = process.env.SECRET;
-const user_model = require('../models/user_model');
+const user_model = require('../models/user');
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
 var salt = bcrypt.genSaltSync(saltRounds);
 var jwt = require('jsonwebtoken');
 require('dotenv').config()
 
-var methods = {}
+var signIn = function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
 
-methods.authentication = function(req, res, next) {
+  user_model.findOne({ username: username }, function(err, user) {
+    if(err) res.send(err);
+    if(user) {
+      bcrypt.compare(password, user.password)
+      .then(result => {
+        if(result) {
+          var token = jwt.sign({id: user._id, username: user.username, email: user.email }, process.env.SECRET);
+          res.send({token: token})
+        } else {
+          res.send({ msg: 'Incorrect password' });
+        }
+      })
+      .catch(err => console.log(err))
+    } else res.send({ msg: 'No such user' })
+  })
+}
+
+var signUp = function(req, res, next) {
+  var salt = bcrypt.genSaltSync(saltRounds);
+  var hash = bcrypt.hashSync(req.body.password, salt);
+
+  var newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: hash
+  })
+  newUser.save((err, user) => {
+    if(err) {
+      res.send(err.errors)
+    } else res.send(user)
+  })
+}
+
+var userInfo = function(req, res, next) {
   let token = req.body.token
-
   if(token) {
-    jwt.verify(token, secret, (err, decoded) {
-      if(decoded._id == req.params._id) {
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if(!err) {
+        req.body.creator = decoded.id;
         next()
       } else {
         res.send(err)
       }
     })
   } else {
-    res.send({msg: 'Login Please!'})
+    res.send({msg: 'Not logged in'})
   }
 }
 
-methods.signUp = function(req, res){
+var userData = function(req, res, next) {
+  let token = req.body.token
 
-    var hash = bcrypt.hashSync(req.body.password, salt);
-    console.log('hasnyanyaaaa ',hash);
-    var user = new user_model({
-      username: req.body.username,
-      password: hash,
-      email: req.body.email
+  if(token) {
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if(!err) {
+        res.send(decoded)
+      } else {
+        res.send(err)
+      }
     })
-    user.save(function(err,result){
-      console.log('usernya', user);
-      if(!err) res.send('success \n'+result)
-      else res.send(err.message)
-    })
+  } else {
+    res.send({msg: 'Not logged in'})
+  }
 }
 
-methods.signIn = function(req, res) {
-  let username = req.body.username
-  let password = req.body.password
-  user_model.find({username: username}, function(err, result) {
-    if (bcrypt.compare(req.body.password, result.password)) {
-      .then(result_data => {
-        if (result_data) {
-          var token = jwt.sign({name: result_data.name, username: result_data.username, email: result_data.email}, secret)
-          res.send({token:token})
-        } else {
-          res.send({msg: 'Failed Check Your Password! '})
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    } else {
-      res.send('user not found!')
-    }
-  })
+//userAuth for reply
+var userAuth = function(req, res, next) {
+  let token = req.body.token
+  console.log(token);
+  if(token) {
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      console.log('ini decode nya --->>',decoded);
+      if(decoded.id == req.params.id) {
+        next()
+      } else {
+        res.send(err)
+      }
+    })
+  } else {
+    res.send({msg: 'Not logged in'})
+  }
 }
 
-module.exports = methods
+module.exports = {
+  signIn, signUp, userInfo, userAuth, userData
+};
